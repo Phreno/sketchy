0101bcdfb6e8b6599a50bd9f3416c581c2191a46
K3rnelP4n1k <Phreno@users.noreply.github.com>
2022-06-26 11:45:02 +0000

    ajoute la possibilité d’extraire les points d’un path

--- spec/SvgWorker.js
+++ spec/SvgWorker.js
@@ -1,14 +1,25 @@
 
-let fxParser = require("fast-xml-parser")
-module.exports = function SvgWorker(){
+const { createSVGWindow } = require('svgdom')
+const window = createSVGWindow()
+const SVG = require("svg.js")(window)
+const fxParser = require("fast-xml-parser")
+const document = window.document
+const draw = SVG(document.documentElement)
+
+module.exports = function SvgWorker() {
     self = {}
-    const parser =new fxParser.XMLParser({
+    const parser = new fxParser.XMLParser({
         ignoreAttributes: false
     })
     self.parse = (svg) => parser.parse(svg)
-    self.getPoints = function getPoints(str)
-    {
-        return []
+    self.getPoints = function getPoints(str) {
+        const path = draw.path(str)
+        const step = 10
+        const length = path.length()
+        return [...Array(Math.ceil(length / step)).keys()].map(i => {
+            const point = path.pointAt(i * step)
+            return [point.x, point.y]
+        })
     }
     return self
 }
--- spec/SvgWorkerSpec.js
+++ spec/SvgWorkerSpec.js
@@ -43,15 +43,27 @@ describe("SvgWorker", () => {
         })
         )
     })
+
     describe("getPoints", () => {
         let data
         beforeEach(() => data = svgWorker.parse(FEED_SYNC_SVG))
         it("doit être une fonction", () => expect(svgWorker.getPoints).toEqual(jasmine.any(Function)));
-        it("doit retourner un tableau", () => {
-
-            expect(svgWorker.getPoints(
-                data.svg.g.path[0]["@_d"])).toEqual(jasmine.any(Array))
-
-        });
+        it("doit retourner un tableau", () => expect(svgWorker.getPoints(data.svg.g.path[0]["@_d"])).toEqual(jasmine.any(Array)));
+        it("doit retourner un tableau correspondant au bon path avec FEED_SYNC_SVG", () => expect(svgWorker.getPoints(data.svg.g.path[0]["@_d"])).toEqual(
+            [[22, 37],
+            [31.47085206716809, 38.486799420560374],
+            [40.570470341169276, 42.599794182318085],
+            [48.77774957211836, 48.81787903538796],
+            [55.571584510130236, 56.6199487298849],
+            [60.43086990531974, 65.4848980159237],
+            [62.834500507801735, 74.89162164361926],
+            [56.272669936205894, 78],
+            [51.29792013785348, 72.45520075563265],
+            [47.15612033998844, 63.2857135831666],
+            [40.242746879632165, 55.5475557621028],
+            [31.649908344011934, 50.1508177817974],
+            [22.46971332035504, 48.00559013160644],
+            [22, 38.51637732722426]]
+        ))
     })
 })
