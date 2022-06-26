7c4cc55a900a35f8b6cb6e240df7736b17878b05
K3rnelP4n1k <Phreno@users.noreply.github.com>
2022-06-26 09:24:52 +0000

    la fonction parse retourne un objet

--- spec/SvgWorker.js
+++ spec/SvgWorker.js
@@ -1,6 +1,9 @@
 module.exports = function SvgWorker(){
     self = {}
     self.parse = function(svg){
+        return {
+
+        }
     }
     return self
 }
--- spec/SvgWorkerSpec.js
+++ spec/SvgWorkerSpec.js
@@ -10,5 +10,6 @@ describe("SvgWorker", ()=>{
     it("doit être un objet", ()=> expect(svgWorker).toEqual(jasmine.any(Object)));
     describe("parse", ()=>{
         it("doit être une fonction", ()=> expect(svgWorker.parse).toEqual(jasmine.any(Function)));
+        it("doit retourner un objet", ()=> expect(svgWorker.parse("<svg></svg>")).toEqual(jasmine.any(Object)));
     })
 })
