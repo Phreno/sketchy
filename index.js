10e770a9bc5bc10e494aba28726d3a76aab833f0
K3rnelP4n1k <Phreno@users.noreply.github.com>
2022-06-29 16:08:00 +0000

    refacto

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ program
     .version(package.version)
     .description("A tool to generate a sketchy stroke")
     .option('--input <file>', 'input file')
+    .option('--output <file>', 'output file or stdout if not specified', false)
     .option('--log <level>', 'log level', 'none')
     .option('--noise <number>', 'Add a random zigzag to the stroke', 0)
     // perfect-freehand options
-    .option('--stroke-size <number>', 'The base size (diameter) of the stroke.', 10)
+    .option('--stroke-size <number>', 'The base size (diameter) of the stroke.', 2)
     .option('--stroke-thinning <number>', 'The effect of pressure on the stroke\'s size.', 0.5)
     .option('--stroke-smoothing <number>', 'How much to soften the stroke\'s edges.', 0.5)
     .option('--stroke-streamline <number>', 'How much to streamline the stroke.', 0.5)
@@ -44,7 +45,6 @@ else if (!fs.existsSync(options.input)) {
 }
 
 LOGGER.level = options.log
-
 LOGGER.info("Reading input file " + options.input)
 const svgString = fs.readFileSync(options.input, 'utf8')
 LOGGER.info("Parsing input file " + options.input)
@@ -68,15 +68,20 @@ const strokes = weaves.map(weave => freehand.getStroke(weave), {
     simulatePressure: options.strokeSimulatePressure,
     last: options.strokeLast,
 })
-LOGGER.info("Generating output file")
+LOGGER.info("Rendering svg from sketchy stroke")
 const svg = [
     "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='100%' height='100%'>",
-    "<g>",
     ...strokes.map(stroke => `<path d='${sketchy.getSvgPathFromStroke(stroke)}'/>`),
-    "</g>",
     "</svg>"
 
 ].join("\n")
-LOGGER.info("Writing output file to stdout")
-console.log(svg)
-exit(1)
+
+if(options.output) {
+    LOGGER.info("Writing output file " + options.output)
+    fs.writeFileSync(options.output, svg)
+
+} else {
+    LOGGER.info("Writing output file to stdout")
+    console.log(svg)
+}
+exit(0)
