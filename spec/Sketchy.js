10a86f1254f800cd837ec1d6d129b87e459d1da9
K3rnelP4n1k <Phreno@users.noreply.github.com>
2022-06-29 09:01:19 +0000

    refacto

--- spec/Sketchy.js
+++ spec/Sketchy.js
@@ -19,15 +19,11 @@ const logger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: { service: 'Sketchy' },
     transports: [
-      //
-      // - Write all logs with importance level of `error` or less to `error.log`
-      // - Write all logs with importance level of `info` or less to `combined.log`
-      //
-    new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
-      new winston.transports.File({ filename: 'error.log', level: 'error' }),
-      new winston.transports.File({ filename: 'combined.log' }),
+        new winston.transports.File({ filename: 'debug.log', level: 'debug' }),
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' }),
     ],
-  });
+});
 
 module.exports = function Sketchy() {
     const self = {}
