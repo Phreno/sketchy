05417e93cea15534383af2b643cbbe7d6c4ef6bc
K3rnelP4n1k <Phreno@users.noreply.github.com>
2022-06-29 16:27:38 +0000

    refacto

--- index.js
+++ index.js
@@ -14,17 +14,17 @@ program
     .name("sketchy")
     .version(package.version)
     .description("A tool to generate a sketchy stroke")
-    .option('--input <file>', 'input file')
-    .option('--output <file>', 'output file or stdout if not specified', false)
-    .option('--log <level>', 'log level', 'none')
-    .option('--noise <number>', 'Add a random zigzag to the stroke', 0)
+    .option('--input                    <file>', 'input file')
+    .option('--output                   <file>', 'output file', 'out.svg')
+    .option('--log                      <level>', 'log level', 'none')
+    .option('--noise                    <number>', 'Add a random zigzag to the stroke', 0)
     // perfect-freehand options
-    .option('--stroke-size <number>', 'The base size (diameter) of the stroke.', 2)
-    .option('--stroke-thinning <number>', 'The effect of pressure on the stroke\'s size.', 0.5)
-    .option('--stroke-smoothing <number>', 'How much to soften the stroke\'s edges.', 0.5)
-    .option('--stroke-streamline <number>', 'How much to streamline the stroke.', 0.5)
+    .option('--stroke-size              <number>', 'The base size (diameter) of the stroke.', 8)
+    .option('--stroke-thinning          <number>', 'The effect of pressure on the stroke\'s size.', 0.5)
+    .option('--stroke-smoothing         <number>', 'How much to soften the stroke\'s edges.', 0.5)
+    .option('--stroke-streamline        <number>', 'How much to streamline the stroke.', 0.5)
     .option('--stroke-simulate-pressure <boolean>', 'Whether to simulate pressure based on velocity.', true)
-    .option('--stroke-last <boolean>', 'Whether the stroke is complete.', false)
+    .option('--stroke-last              <boolean>', 'Whether the stroke is complete.', false)
 
     .parse(process.argv)
 
@@ -57,31 +57,26 @@ LOGGER.info("Generating sketchy stroke from " + options.input)
 const breadcrumbs = paths.map(path => sketchy.getPointsFromSvgPath(path))
 LOGGER.info("Add some noise sketchy stroke " + options.noise)
 let weaves = options.noise
-    ?  breadcrumbs.map(breadcrumb => sketchy.randomize(breadcrumb, { noise: options.noise }))
+    ? breadcrumbs.map(breadcrumb => sketchy.randomize(breadcrumb, { noise: options.noise }))
     : breadcrumbs
 LOGGER.info("Generating freehand stroke")
+console.log(options)
 const strokes = weaves.map(weave => freehand.getStroke(weave), {
-    size: options.strokeSize,
-    thinning: options.strokeThinning,
-    smoothing: options.strokeSmoothing,
-    streamline: options.strokeStreamline,
+    size: parseFloat(options.strokeSize),
+    thinning: parseFloat(options.strokeThinning),
+    smoothing: parseFloat(options.strokeSmoothing),
+    streamline: parseFloat(options.strokeStreamline),
     simulatePressure: options.strokeSimulatePressure,
     last: options.strokeLast,
 })
 LOGGER.info("Rendering svg from sketchy stroke")
 const svg = [
-    "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='100%' height='100%'>",
+    "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>",
     ...strokes.map(stroke => `<path d='${sketchy.getSvgPathFromStroke(stroke)}'/>`),
     "</svg>"
-
 ].join("\n")
 
-if(options.output) {
-    LOGGER.info("Writing output file " + options.output)
-    fs.writeFileSync(options.output, svg)
+LOGGER.info("Writing output file")
+fs.writeFileSync(options.output, svg, { encoding: 'utf8' })
 
-} else {
-    LOGGER.info("Writing output file to stdout")
-    console.log(svg)
-}
 exit(0)
