90438768eef98207ffffd6898fda5e83a5f94659
K3rnelP4n1k <Phreno@users.noreply.github.com>
2022-07-15 08:51:56 +0000

    actualise le prototype

--- dist/scribble.js
+++ dist/scribble.js
@@ -4,23 +4,77 @@ const { createSVGWindow } = require('svgdom')
 const window = createSVGWindow()
 const document = window.document
 const { SVG, registerWindow } = require('@svgdotjs/svg.js')
-const { Layer} = require("../tools/cross-hatching/script.js")
-
+const { Layer } = require("../tools/Layer.js")
 // register window and document
 registerWindow(window, document)
-const draw = SVG(document.documentElement)
- fs = require("fs")
+fs = require("fs")
 const inkjet = require('inkjet');
+const { exit } = require('process')
+const fxParser = require('fast-xml-parser')
+const Sketchy = require('../spec/Sketchy')
+const parser = new fxParser.XMLParser({ ignoreAttributes: false })
+const freehand = require('perfect-freehand')
+const Jimp = require('jimp')
+
+const sketchy = new Sketchy()
 
 
+const div = document.createElementNS('http://www.w3.org/2000/svg','svg')
+const draw = SVG(div)
+//draw.viewbox(0,0,1,1)
+draw.css({
+  stroke: "black",
+  "stroke-width": "0.0002"
+})
 
-// document.documentElement.innerHTML = draw.svg();
 
-inkjet.decode(fs.readFileSync('./tools/cross-hatching/car.jpg'), function(err, decoded) {
-	for (let i = 0; i < 5; i++) {
-		let layer = new Layer(document.getElementsByTagName("svg")[0], decoded);
-		layer.drawPattern((255 / 5) * i, 0.01, Math.cos(i), Math.sin(i));
-	}
-  draw.svg(document.documentElement.innerHTML)
-  console.log(draw.svg())
-});
+inkjet.decode(fs.readFileSync('./rsc/tpdne.jpeg'), function (err, imageData) {
+  if (err) {
+    console.log(err)
+    exit(1)
+  } else {
+    for (let i = 0; i < 5; i++) {
+      let layer = new Layer(div, imageData);
+      layer.drawPattern((255 / 5) * i, 0.005, Math.cos(i), Math.sin(i));
+    }
+  }
+})
+
+
+// parse Svg String
+let svgDocument = parser.parse(draw.svg())
+let lines = sketchy.getLinesFromSvg(svgDocument)
+console.log(lines.length + " lines found")
+// remove duplicate lines
+lines = lines.reduce((acc, line) => {
+    const alreadyExists = acc.some(dirty => Object.keys(dirty).every(keys => dirty[keys] === line[keys]))
+    if (!alreadyExists) acc.push(line)
+    return acc
+}, [])
+
+// scale lines
+lines = lines.map(line => ({
+  "@_x1": line["@_x1"] * 10000,
+  "@_y1": line["@_y1"] * 10000,
+  "@_x2": line["@_x2"] * 10000,
+  "@_y2": line["@_y2"] * 10000
+}))
+
+
+// segment lines into array of arrays of points
+lines = lines.map(line => `M${line["@_x1"]} ${line["@_y1"]}, L${line["@_x2"]} ${line["@_y2"]}`)
+lines = lines.map(line => sketchy.getPointsFromSvgPath(line, 10))
+lines = lines.map(line => sketchy.randomize(line, {noise: 10}))
+
+let strokes = lines.map(line => freehand.getStroke(line))
+let paths = strokes.map(stroke => sketchy.getSvgPathFromStroke(stroke))
+
+const svg = [
+  "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>",
+  "<g>",
+  ...paths.map(path => `<path d='${path}'/>`),
+  "</g>",
+  "</svg>"
+].join("\n")
+
+fs.writeFileSync("rsc/out.svg", svg, { encoding: 'utf8' })
