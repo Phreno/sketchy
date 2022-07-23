87b9a8bdbc9e059b00a346bd2162463598ab0fd7
unknown <unknown>
2022-07-23 07:52:01 +0200

    on laisse le temps au soft d'inverser l'image

--- dist/scribble.js
+++ dist/scribble.js
@@ -35,7 +35,7 @@ program
 
     .option("-Y, --layers <number>", "number of layer decomposition", 5)
     .option("-W, --distance <number>", "distance between strokes", 0.005)
-
+    .option("-Tr, --trace <string>", "trace algorithm", "posterized1")
     .option("-I, --ignore-layer <string>", "layer to ignore", false)
 
     .option('-C, --last', 'whether the stroke is complete')
@@ -61,8 +61,6 @@ draw.css({
 })
 
 var Jimp = require('jimp');
-const PathSplitter = require('../tools/PathSplitter.js')
-
 //options.input = "./rsc/tpdne.jpeg"
 
 let filename = options.input.split("/").pop()
@@ -72,85 +70,88 @@ let buffer = "./rsc/"+filename+"-invert."+extension
 
 // User-Defined Function to read the images
 async function main() {
-  await Jimp.read(options.input).then(image=>
-    image.invert().write(buffer));
+  console.log("reading image")
+  return await Jimp.read(options.input).then(image=>
+    image.invert().writeAsync(buffer).then(()=>{
+      console.log("image inverted")
+      let svgTrace, image
+      inkjet.decode(fs.readFileSync(buffer), function (err, imageData) {
+        image = imageData
+        if (err) {
+          console.log(err)
+          exit(1)
+        } else {
+          for (let i = 0; i < options.layers; i++) {
+            // todo: ignorer plusieurs calques
+            if(options.ignoreLayer && options.ignoreLayer.split(",").includes(""+i)) continue;
+              let layer = new Layer(div, imageData);
+              layer.drawPattern(
+                (255 / options.layers) * i,
+                // todo: ajuster la distance suivant le layer
+                options.distance,
+                // todo: possibilité de randomizer la direction
+                Math.cos(i),
+                Math.sin(i));
+          }
+
+
+          svgTrace = parser.parse(ImageTracer.imagedataToSVG(imageData, options.trace))
+        }
+      })
+
+      let pathTrace = sketchy.getPathsFromSvg(svgTrace)
+      pathTrace = pathTrace.map(path=>pathSplitter((path))).flat()
+      let pointsTrace = pathTrace.map(path => sketchy.getPointsFromSvgPath(path, options.stepSize))
+      pointsTrace = pointsTrace.map(breadcrumb => sketchy.randomize(breadcrumb, { noise: options.noise }))
+      let trace = pointsTrace.map(weave => freehand.getStroke(weave, options))
+
+
+
+      // parse Svg String
+      let svgDocument = parser.parse(draw.svg())
+      fs.writeFileSync("out.svg", JSON.stringify(svgDocument, null, 2))
+      let lines = sketchy.getLinesFromSvg(svgDocument)
+      console.log(lines.length + " lines found")
+      // remove duplicate lines
+      lines = lines.reduce((acc, line) => {
+          const alreadyExists = acc.some(dirty => Object.keys(dirty).every(keys => dirty[keys] === line[keys]))
+          if (!alreadyExists) acc.push(line)
+          return acc
+      }, [])
+
+      // scale lines
+      // todo: faire des constantes
+      lines = lines.map(line => ({
+        "@_x1": line["@_x1"] * image.width + Math.random() * options.noise,
+        "@_y1": line["@_y1"] * image.height + Math.random() * options.noise,
+        "@_x2": line["@_x2"] * image.width + Math.random() * options.noise,
+        "@_y2": line["@_y2"] * image.height + Math.random() * options.noise
+      }))
+
+
+      // segment lines into array of arrays of points
+      lines = lines.map(line => `M${line["@_x1"]} ${line["@_y1"]}, L${line["@_x2"]} ${line["@_y2"]}`)
+      lines = lines.map(line => sketchy.getPointsFromSvgPath(line, options.stepSize))
+      lines = lines.map(line => sketchy.randomize(line, options))
+
+      let strokes = lines.map(line => freehand.getStroke(line, options))
+      let paths = strokes.map(stroke => sketchy.getSvgPathFromStroke(stroke))
+
+      const svg = [
+        "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>",
+        "<g style=\"fill: transparent; stroke: black; stroke-width: 0.5\">",
+        ...paths.map(path => `<path d="${path}"/>`),
+        "</g>",
+        "<g style=\"fill: transparent; stroke: black; stroke-width: 0.5\">",
+        ...trace.map(path=> `<path d="${sketchy.getSvgPathFromStroke(path)}"/>`),
+        "</g>",
+        "</svg>"
+      ].join("\n")
+
+      fs.writeFileSync("rsc/"+filename+".svg", svg, { encoding: 'utf8' })
+      exit(0)
+
+    }));
 }
 
 main()
-
-let svgTrace, image
-inkjet.decode(fs.readFileSync(buffer), function (err, imageData) {
-  image = imageData
-  if (err) {
-    console.log(err)
-    exit(1)
-  } else {
-    for (let i = 0; i < options.layers; i++) {
-      // todo: ignorer plusieurs calques
-      if(options.ignoreLayer && options.ignoreLayer.split(",").includes(""+i)) continue;
-        let layer = new Layer(div, imageData);
-        layer.drawPattern(
-          (255 / options.layers) * i,
-          // todo: ajuster la distance suivant le layer
-          options.distance,
-          // todo: possibilité de randomizer la direction
-          Math.cos(i),
-          Math.sin(i));
-    }
-
-
-    svgTrace = parser.parse(ImageTracer.imagedataToSVG(imageData, "posterized1"))
-  }
-})
-
-let pathTrace = sketchy.getPathsFromSvg(svgTrace)
-pathTrace = pathTrace.map(path=>pathSplitter((path))).flat()
-let pointsTrace = pathTrace.map(path => sketchy.getPointsFromSvgPath(path, options.stepSize))
-pointsTrace = pointsTrace.map(breadcrumb => sketchy.randomize(breadcrumb, { noise: options.noise }))
-let trace = pointsTrace.map(weave => freehand.getStroke(weave, options))
-
-fs.writeFileSync("out.svg", JSON.stringify(trace, null, 2))
-
-
-// parse Svg String
-let svgDocument = parser.parse(draw.svg())
-let lines = sketchy.getLinesFromSvg(svgDocument)
-console.log(lines.length + " lines found")
-// remove duplicate lines
-lines = lines.reduce((acc, line) => {
-    const alreadyExists = acc.some(dirty => Object.keys(dirty).every(keys => dirty[keys] === line[keys]))
-    if (!alreadyExists) acc.push(line)
-    return acc
-}, [])
-
-// scale lines
-// todo: faire des constantes
-lines = lines.map(line => ({
-  "@_x1": line["@_x1"] * image.width + Math.random() * options.noise,
-  "@_y1": line["@_y1"] * image.height + Math.random() * options.noise,
-  "@_x2": line["@_x2"] * image.width + Math.random() * options.noise,
-  "@_y2": line["@_y2"] * image.height + Math.random() * options.noise
-}))
-
-
-// segment lines into array of arrays of points
-lines = lines.map(line => `M${line["@_x1"]} ${line["@_y1"]}, L${line["@_x2"]} ${line["@_y2"]}`)
-lines = lines.map(line => sketchy.getPointsFromSvgPath(line, options.stepSize))
-lines = lines.map(line => sketchy.randomize(line, options))
-
-let strokes = lines.map(line => freehand.getStroke(line, options))
-let paths = strokes.map(stroke => sketchy.getSvgPathFromStroke(stroke))
-
-const svg = [
-  "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink'>",
-  "<g style=\"fill: transparent; stroke: black; stroke-width: 0.5\">",
-  ...paths.map(path => `<path d="${path}"/>`),
-  "</g>",
-  "<g style=\"fill: transparent; stroke: black; stroke-width: 0.5\">",
-  ...trace.map(path=> `<path d="${sketchy.getSvgPathFromStroke(path)}"/>`),
-  "</g>",
-  "</svg>"
-].join("\n")
-
-fs.writeFileSync("rsc/"+filename+".svg", svg, { encoding: 'utf8' })
-exit(0)
