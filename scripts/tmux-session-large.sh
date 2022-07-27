02425a52ce59ccc8ca690530a4c93b98beee7364
phreno <losgoomy@gmail.com>
2022-07-27 23:08:23 +0200

    ajoute de la config pour tmux

--- scripts/tmux-session-large.sh
+++ scripts/tmux-session-large.sh
@@ -1,5 +1,8 @@
 
 #! /bin/sh
+backgroundSession="sketchy-background"
+backgroundWindow="$backgroundSession:0"
+
 session="sketchy"
 window="$session:0"
 
@@ -8,19 +11,24 @@ if [ $(tmux attach -t "$session") ]; then
 fi
 
 cd ~/git/sketchy
+
+# Tache de fond
+tmux new-session -d -s "$backgroundSession"
+tmux send-keys -t "$backgroundWindow.0" "npm run ctags:watch" C-m
+
+
+# Affichage principal
+
 tmux new-session -d -s "$session"
 
-tmux split-window -t "$window"
 tmux split-window -t "$window"
 tmux split-window -t "$window"
 tmux select-layout -t "$window" main-vertical
 
 tmux send-keys -t "$window.0" "npm run edit" C-m
-tmux send-keys -t "$window.1" "npm run ctags:watch" C-m
-tmux send-keys -t "$window.2" "npm run test:watch" C-m
-tmux send-keys -t "$window.3" "npm run status:watch" C-m
+tmux send-keys -t "$window.1" "npm run test:watch" C-m
+tmux send-keys -t "$window.2" "npm run status:watch" C-m
 
 tmux select-pane -t "$window.0"
-tmux send-keys -t "$window.0" ":CHADopen" C-m
 
 tmux attach -t "$session"
